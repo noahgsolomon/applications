1653f1ce014bf56c85e4b25cf8d01cbae9677175
Noah Solomon <noahsolomon2003@gmail.com>
2024-07-26 20:42:56 -0400

    oops...

--- sst.env.ts
+++ sst.env.ts
@@ -1,5 +1,5 @@
 export const NextEnv = {
   DB_URL: process.env.DB_URL!,
   NEXTAUTH_URL: "https://d1rqn3zg0qe03m.cloudfront.net",
-  NEXAUTH_SECRET: process.env.NEXAUTH_SECRET!,
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
 };
