e4e3dd7b5eae5b793bd3356e47c24de34f4f6c6d
Noah Solomon <noahsolomon2003@gmail.com>
2024-08-05 14:35:46 -0400

    candidate card

---
+++ app/candidate-card.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { Card, Heading, Text, Link, Avatar, Badge } from "frosted-ui";
+import { SquareArrowOutUpRight } from "lucide-react";
+
+export default function CandidateCard({
+  candidate,
+  outbound,
+}: {
+  candidate: Candidate;
+  outbound: Outbound;
+}) {
+  return (
+    <Card>
+      <div className="flex flex-row  gap-2 pb-4">
+        <Avatar
+          size={"5"}
+          color="blue"
+          fallback={candidate.linkedinData.firstName.toUpperCase().charAt(0)}
+          src={candidate.linkedinData.photoUrl ?? ""}
+        />
+        <div className="flex flex-col ">
+          <div className="flex flex-row gap-2 items-center">
+            <Heading>
+              {candidate.linkedinData.firstName +
+                " " +
+                candidate.linkedinData.lastName}
+            </Heading>
+            <Link
+              href={candidate.url!}
+              target="_blank"
+              className="cursor-pointer"
+              key={candidate.id}
+            >
+              <SquareArrowOutUpRight className="size-4" />
+            </Link>
+          </div>
+          <div className="flex flex-row gap-1">
+            <Text>
+              {candidate.linkedinData.positions.positionHistory[0].companyName}
+            </Text>
+
+            <Text>
+              {candidate.linkedinData.positions.positionHistory[0].title}
+            </Text>
+          </div>
+          <Text className="italic text-sm text-primary/60">
+            {candidate.miniSummary}
+          </Text>
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-2 pb-4">
+        <Badge
+          variant="surface"
+          color={candidate.livesNearBrooklyn ? "green" : "red"}
+        >
+          Brooklyn
+        </Badge>
+        <Badge
+          variant="surface"
+          color={candidate.workedInBigTech ? "green" : "red"}
+        >
+          Big Tech
+        </Badge>
+        <Badge
+          variant="surface"
+          color={candidate.workedAtRelevant ? "green" : "red"}
+        >
+          {outbound.company}
+        </Badge>
+        <Badge
+          variant="surface"
+          color={candidate.workedInPosition ? "green" : "red"}
+        >
+          {outbound.job}
+        </Badge>
+      </div>
+    </Card>
+  );
+}
