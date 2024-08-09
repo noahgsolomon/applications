c772d6806731ef7b4ee10a70ffb02667d7cc326b
Noah Solomon <noahsolomon2003@gmail.com>
2024-08-09 14:54:18 -0400

    + polling company outbound

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import OutboundDialog from "./outbound-dialog";
 import CompanyDialog from "./company-dialog";
 import PreviousOutboundSearchesButton from "./history/previous-outbound-searches-button";
 import ListeningCompanies from "./companies-listening";
+import PollingCompanyOutbound from "./polling-company-outbound";
 
 export default async function Home() {
   // const user = await api.user.me();
@@ -9,6 +10,7 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col gap-12 items-center justify-center w-full h-[100vh]">
+      <PollingCompanyOutbound />
       <OutboundDialog />
       <CompanyDialog />
       <ListeningCompanies />
