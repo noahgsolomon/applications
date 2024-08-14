74588730b425c98629c5b899d7589ecce1b5596d
Noah Solomon <noahsolomon2003@gmail.com>
2024-08-14 17:10:12 -0400

    invoke action

--- sst.config.ts
+++ sst.config.ts
@@ -10,6 +10,7 @@ export default $config({
     };
   },
   async run() {
+    //
     // const hono = new sst.aws.Function("PoliticalBlogHono", {
     //   environment: HonoEnv,
     //   url: true,
