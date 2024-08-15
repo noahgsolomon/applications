e67401cf409b74a02e671982cfb1be78cb7a93c2
Noah Solomon <noahsolomon2003@gmail.com>
2024-08-15 17:41:42 -0400

    jsobArrayContains for logical AND and Any for logial OR

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,33 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { SQL, sql, AnyColumn, SQLChunk } from "drizzle-orm";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function jsonArrayContainsAny<T extends AnyColumn>(
+  column: T,
+  values: T["_"]["data"],
+): SQL {
+  return sql`
+    EXISTS (
+      SELECT 1
+      FROM json_array_elements_text(${column}::json) AS elem
+      WHERE elem = ANY(ARRAY[${sql.join(values as SQLChunk[], sql`,`)}])
+    )
+  `;
+}
+
+export function jsonArrayContains<T extends AnyColumn>(
+  column: T,
+  values: string[],
+): SQL {
+  return sql`
+    (
+      SELECT COUNT(*) = ${values.length}
+      FROM json_array_elements_text(${column}::json) AS elem
+      WHERE elem = ANY(ARRAY[${sql.join(values as SQLChunk[], sql`,`)}])
+    )
+  `;
 }
