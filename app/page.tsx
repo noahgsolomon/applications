04d4ebec32ce56c3fa69131d7cf11e5bc587dfd9
Noah Solomon <noahsolomon2003@gmail.com>
2024-08-15 17:41:28 -0400

    scraped dialog on page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import CompanyDialog from "./company-dialog";
 import PreviousOutboundSearchesButton from "./history/previous-outbound-searches-button";
 import ListeningCompanies from "./companies-listening";
 import PollingCompanyOutbound from "./polling-company-outbound";
+import RecommendedOutboundResults from "./recommended-outbound-results";
+import ScrapedDialog from "./scraped-dialog";
 
 export default async function Home() {
   // const user = await api.user.me();
@@ -10,11 +12,13 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col gap-12 items-center justify-center w-full h-[100vh]">
-      <PollingCompanyOutbound />
-      <OutboundDialog />
-      <CompanyDialog />
+      {/* <PollingCompanyOutbound /> */}
+      {/* <OutboundDialog /> */}
+      {/* <CompanyDialog /> */}
+      <ScrapedDialog />
       <ListeningCompanies />
       <PreviousOutboundSearchesButton />
+      {/* <RecommendedOutboundResults /> */}
     </div>
   );
 }
