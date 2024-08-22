aca19ec91c33ac9f03cf0aa34c93c391618e7a59
Noah Solomon <noahsolomon2003@gmail.com>
2024-08-22 12:08:05 -0400

    filter store

---
+++ app/store/filter-store.ts
@@ -0,0 +1,20 @@
+import { api as ServerApi } from "@/trpc/server";
+import { create } from "zustand";
+
+export type CompanyFilterReturnType = Awaited<
+  ReturnType<typeof ServerApi.outbound.companyFilter>
+>;
+
+type ScrapedDialogStore = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  filters: CompanyFilterReturnType | null;
+  setFilters: (filters: CompanyFilterReturnType | null) => void;
+};
+
+export const useScrapedDialogStore = create<ScrapedDialogStore>((set) => ({
+  open: false,
+  setOpen: (open) => set({ open }),
+  filters: null,
+  setFilters: (filters) => set({ filters }),
+}));
