dd85f9c89aa66ae0c2d7c977a6f3f63ea1f08e84
Noah Solomon <noahsolomon2003@gmail.com>
2024-08-29 11:58:01 -0400

    logic to add cookd review

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,16 +1,50 @@
 // app/api/webhook/route.ts
 
 import { NextRequest, NextResponse } from "next/server";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import dotenv from "dotenv";
+import * as userSchema from "../../../server/db/schemas/users/schema";
+import { eq } from "drizzle-orm";
+
+dotenv.config({
+  path: "../.env",
+});
+
+const connection = neon(process.env.DB_URL!);
+
+const db = drizzle(connection, {
+  schema: {
+    ...userSchema,
+  },
+});
 
 export async function POST(req: NextRequest) {
   try {
     console.log("Received POST request");
     const bodyText = await req.text();
-    console.log("Request body:", bodyText);
+
+    let id: string;
+    let cookdData: { score: any; result: string; resumeScreenerId: string };
+    let cookdScore: number;
 
     try {
       const bodyJson = JSON.parse(bodyText);
-      console.log("Parsed JSON body:", bodyJson);
+
+      id = bodyJson.candidateJson.id;
+      cookdData = {
+        score: bodyJson.score,
+        result: bodyJson.result,
+        resumeScreenerId: bodyJson.resumeScreenerId,
+      };
+      cookdScore = Number(bodyJson.score.numericScore);
+
+      await db
+        .update(userSchema.candidates)
+        .set({ cookdData, cookdScore, cookdReviewed: true })
+        .where(eq(userSchema.candidates.id, id));
+
+      console.log(`Updated Cookd data for ${id}`);
     } catch (parseError) {
       console.log("Body is not valid JSON");
     }
