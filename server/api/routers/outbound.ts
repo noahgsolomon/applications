0a5007c30f2c9fc9292d093e839569305a1fb092
Noah Solomon <noahsolomon2003@gmail.com>
2024-09-12 11:20:03 -0400

    all at once

--- server/api/routers/outbound.ts
+++ server/api/routers/outbound.ts
@@ -475,7 +475,7 @@ export async function fetchCandidatesWithCursor(
           ),
         ),
       ),
-      limit: 10000,
+      limit: 100000,
       orderBy: [asc(schema.candidates.createdAt), asc(schema.candidates.id)],
     });
     return candidates;
